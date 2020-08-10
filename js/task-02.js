47e52494327d1d9113f139c42adcecd2a0c37434
Serhii Malyshko <malyshkosergiy@gmail.com>
2020-08-10 19:57:56 +0300

    task 2 is added

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,11 @@
 import users from "./users.js";
+
+const getUsersWithEyeColor = (users, color) => {
+  const usersEyeColor = users.filter(function (user) {
+    return user.eyeColor === "blue";
+  });
+
+  return usersEyeColor;
+};
+
+console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
