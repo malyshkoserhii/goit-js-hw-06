3d71fa2fc6f3a69d0eb23b28caa93f75c992796b
Serhii Malyshko <malyshkoserhii91@gmail.com>
2020-08-12 18:24:10 +0300

    task 8 is added

--- js/task-08.js
+++ js/task-08.js
@@ -1,12 +1,18 @@
 import users from "./users.js";
 
 const getUsersWithFriend = (users, friendName) => {
-  const userFriends = users.reduce(function (acc, user) {
-    return user.name;
-  });
+  const userFriends = users
+    .filter(function (user) {
+      if (user.friends.includes(friendName)) {
+        return user;
+      }
+    })
+    .map(function (user) {
+      return user.name;
+    });
 
   return userFriends;
 };
 
-console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Briana Decker"));
+console.log(getUsersWithFriend(users, "Goldie Gentry"));
